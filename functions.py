69c30929edcd0613a4f697aae82547fc4df587d2
Amos Mugabi <115138015+Amos-Mugabi@users.noreply.github.com>
2023-02-01 16:11:17 +0300

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,60 @@
+"""
+Functions
+ a reusable block of code tht performs a specific task and returns a result.
+2 types:
+    std library fxn
+    user-defined fxn
+other types:
+    anonymous fxns
+    recursive fxns
+    higher-order fxns
+print(),len(),int(), str(), max(), min(), sum(), sorted(), range(), abs()
+
+"""
+"""
+#Structure of a fxn.
+#Defn
+def product():
+    return 18*3
+#calling the fxn
+print(product())
+"""
+
+
+"""
+def sum_of_numbers(num1, num2):
+    return num1 + num2
+print(sum_of_numbers(6,3))
+"""
+
+#a program tht prints even numbers up to 8 & allows a
+#user to check if their choice of numbers is even.
+
+"""
+def is_even(number):
+    if number%2 == 0:
+        return True
+    else : return False
+
+    for i in range(0,10):
+        if is_even(i):
+            print(i)
+"""
+
+ #Lambda fxns
+product = lambda a,b: a*b
+print(product(8,6))
+            
+
+
+
+
+
+
+
+
+
+
+
+
+    
