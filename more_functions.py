fb0049f5cf5b0fd5f47af56f2a330df93e200579
Amos Mugabi <115138015+Amos-Mugabi@users.noreply.github.com>
2023-02-02 11:29:35 +0300

    Add files via upload

---
+++ more_functions.py
@@ -0,0 +1,30 @@
+#Functions
+
+"""a = input("What is your name?")
+print(a)
+
+b = input("What is your name?")
+print(b)
+
+
+c = input("What is your name?")
+print(c)"""
+
+#Defn a function and calling it.
+
+"""def function_name():
+    body
+"""
+def asks_name():
+    name = input("What is your name?")
+    print(name)
+
+    
+
+print("we are starting to call the function here")
+asks_name()
+
+asks_name()
+
+asks_name()
+print("we are ending to call the function here")
