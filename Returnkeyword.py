51db13e80e021d40825362127ab5f3aad2f358f1
Amos Mugabi <115138015+Amos-Mugabi@users.noreply.github.com>
2023-02-10 18:25:32 +0300

    Add files via upload

---
+++ Returnkeyword.py
@@ -0,0 +1,42 @@
+#Return keyword
+
+"""def myfunction():
+    return 3+3
+print(myfunction())
+"""
+
+# Used to exit a function and return a value.
+
+"""
+def myfunction():
+    
+    print("Hi")
+
+
+print("Hello,world")
+
+print(myfunction())
+"""
+
+"""def greet(name):
+    print("Hello", name)
+
+
+greet("Alice")    a function that prints a value.
+
+"""
+"""
+def greet(name):
+    return "Hello" + name
+
+
+greeting = greet("Alice")     # a function that returns a value.
+                             Does not print it.
+
+"""
+def greet(name):
+    return "Hello" +  name
+
+
+greeting = greet("Alice")       # the print() function has now been called.
+print(greeting)
