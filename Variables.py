61a5e4382dee827e897f06a2dd7f9a88d4904164
Amos Mugabi <115138015+Amos-Mugabi@users.noreply.github.com>
2023-02-20 20:14:31 +0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,56 @@
+print("Hello world")
+
+# variable sin python
+
+name = "amos mugabi"
+age = 22 
+gender = "male 🧞‍♂️"
+status = "single"
+
+home_district = "Iganga"
+homECountry = "Uganda"
+
+
+print(name)
+print(age)
+print(gender)
+print(status)
+
+#Rules of declaring variables.
+# Naming variables
+
+#snake_case
+#cameLCase
+
+#MACRO_CASE
+#CapsCase
+
+
+#Avoid using keywords to declare variables.
+
+#LITERALS
+#Numeric literals
+amount = 500
+distance = 12.8
+quantity =  0.6
+#String literals
+student_name = "Amos"
+regNo = "S22813/089"
+
+#Boolean literals
+isResident = "True"
+has_meals = "True"
+#Special literals
+graduation_date = Non
+#Collection literals
+semester-studied = {1, 2, "three"}
+
+
+
+
+
+
+
+
+
+
