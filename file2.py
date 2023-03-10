8980f9fc51f99f9f367899d2137dcab35c88dcc1
Amos Mugabi <115138015+Amos-Mugabi@users.noreply.github.com>
2023-03-10 12:15:36 +0300

    Add files via upload

---
+++ file2.py
@@ -0,0 +1,28 @@
+# classes
+
+class student:
+
+    name = "amos"
+    email = "am@gmail.com"
+
+        # Init method
+    def __init__(self, my_name, my_access, my_regno):
+        self.my_name = my_name
+        self.my_access = my_access
+        self.my_regno = my_regno
+
+
+student1 = student("mark", "A6743", "S472637",)
+print(type(student1))
+
+# the output class __main__.student means that the class object comes
+from the class type (student) 
+
+
+"""def greeting(self):
+    print("What is your name?")
+    """
+
+
+    
+        
